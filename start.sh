4f24ea10d39ae38d3e7f948e611b5ff1d31ce9a9
saisirishaperam <saisirishaa862@gmail.com>
2025-12-19 10:06:17 +0530

    first commit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py collectstatic --noinput 
 python manage.py migrate --noinput 
-gunicorn trinity_clg.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn voterid.wsgi:application --bind 0.0.0.0:$PORT
